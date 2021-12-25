4e1b119a54f84af386ca824b560cec8bd4715ee6
DANIEL CHEN <daniel77chen@hotmail.com>
2021-12-25 16:58:54 -0500

    undo, change last css, can't play past winner

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ var wsServer = new WebSocketServer({
 });
 
 var board = [];
+var moves = [];
 var length = 17
 for (var i = 0 ; i < length ; i++) {
     for (var j = 0 ; j < length ; j++) {
@@ -56,6 +57,7 @@ var startPlayer = 0
 var currentPlayer = 0;
 var replay = [false, false]
 var winner = null
+var undoRequest = false;
 function restartGame() {
     startPlayer = 1 - startPlayer
     currentPlayer = startPlayer
@@ -63,6 +65,7 @@ function restartGame() {
     for (var i = 0 ; i < board.length ; i++) {
         board[i] = null
     }
+    moves = []
 }
 
 function check(cnt) {
@@ -112,12 +115,15 @@ wsServer.on("request", function(request) {
             id = 2;
         }
     }
-    sendData({type: 'start', team: id, board: board, turn: currentPlayer})
+    sendData({type: 'start', team: id, board: board, turn: currentPlayer, last: moves.length ? moves[moves.length - 1].cnt : undefined})
     if (winner !== null) {
         sendData({
             type: "win",
             team: winner
         })
+    }
+    if (undoRequest) {
+        sendData(undoRequest)
     }
     listeners[id] = sendData
     messageHandler = function(message) {
@@ -125,7 +131,11 @@ wsServer.on("request", function(request) {
         var type = query ? query.type : "";
         switch(type) {
             case "play":
-                if (query.team == currentPlayer && board[query.cnt] === null) {
+                if (query.team == currentPlayer && board[query.cnt] === null && winner === null) {
+                    moves.push({
+                        currentPlayer,
+                        cnt: query.cnt,
+                    })
                     board[query.cnt] = query.team
                     var win = check(query.cnt)
                     listeners.forEach((l) => {
@@ -168,6 +178,30 @@ wsServer.on("request", function(request) {
                 currentGameId++;
                 activeGames[currentGameId] = thisGame;
                 joinGame(currentGameId,query.name);
+                break;
+            case "undoRequest":
+                undoRequest = {
+                    type: "undoRequest",
+                    team: query.team,
+                }
+                listeners.forEach((l, i) => {
+                    l && l(undoRequest)
+                })
+                break;
+            case "undo":
+                undoRequest = undefined
+                if (query.answer === "yes") {
+                    board[moves.pop().cnt] = null
+                    currentPlayer = 1 - currentPlayer
+                }
+                listeners.forEach((l, i) => {
+                    l && l({
+                        type: "undo",
+                        answer: query.answer,
+                        last: moves.length ? moves[moves.length-1].cnt : undefined,
+                        team: 1 - currentPlayer
+                    })
+                })
                 break;
         }
     };
